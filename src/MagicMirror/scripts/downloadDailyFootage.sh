a5dec74054250f6f7adbc457b80c5883d2aaa795
Michael Grayston <michael.grayston@tesco.com>
2020-12-20 16:08:50 +0000

    corrections

--- src/MagicMirror/scripts/downloadDailyFootage.sh
+++ src/MagicMirror/scripts/downloadDailyFootage.sh
@@ -1,12 +1,12 @@
 currentDate=$(date  '+%YY%mM%dD')
 
-cd /home/pi/Camera
+cd /home/pi/SecurityCameras
 wget -m --user root --password <PASSWORD> ftp://<SECURITY_CAMERA_IP>//tmp/sd/record/$currentDate*
 sleep 10
-rclone copy /home/pi/Camera/<SECURITY_CAMERA_IP>/tmp/sd/record/ GoogleDrive:CCTV/FrontdoorCamera
+rclone copy /home/pi/SecurityCameras/<SECURITY_CAMERA_IP>/tmp/sd/record/ GoogleDrive:CCTV/FrontdoorCamera
 
 sleep 5
 
 wget -m --user root --password <PASSWORD> ftp://<SECURITY_CAMERA_IP>//tmp/sd/record/$currentDate*
 sleep 10
-rclone copy /home/pi/Camera/<SECURITY_CAMERA_IP>/tmp/sd/record/ GoogleDrive:CCTV/DrivewayCamera
+rclone copy /home/pi/SecurityCameras/<SECURITY_CAMERA_IP>/tmp/sd/record/ GoogleDrive:CCTV/DrivewayCamera
